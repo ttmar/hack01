1cfca777b95decb38c3a295f01eb5e8516cc45f3
Victor Nguyen <minhnguyennhat0108@gmail.com>
2018-08-23 12:20:23 -0500

    hey

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # hack01
+
+My name is Victor Nguyen and Im a computer science major!
+
+Github URL: https://github.com/ttmar/hack01
---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Victor Nguyen & Collin Sipple
+ * Date: 08/22/2018
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Victor Nguyen,Collin Sipple\n");
+
+  return 0;
+}
